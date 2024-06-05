250d754b3acfdc602555542012c9ba86d7034ff4
Sushant Wandhekar <sushantwandhekar4@gmail.com>
2024-06-05 16:19:36 +0530

    Update create_glue_job.py
    Added 
    Check if the Glue job with the specified name already exists.
    If it does not exist, create the Glue job.
    If it does exist, check if the script associated with the existing job matches the new script. If the script is updated, update the Glue job.

--- .github/workflows/create_glue_job.py
+++ .github/workflows/create_glue_job.py
@@ -1,5 +1,13 @@
 import boto3
 
+def job_exists(glue_client, job_name):
+    try:
+        response = glue_client.get_job(JobName=job_name)
+        return True, response['Job']
+    except glue_client.exceptions.EntityNotFoundException:
+        return False, None
+
+
 def create_glue_job(job_name, script_location, role_arn, glue_client,list_connec,extra_files):
     try:
         response = glue_client.create_job(
@@ -11,7 +19,7 @@ def create_glue_job(job_name, script_location, role_arn, glue_client,list_connec
                 'PythonVersion': '3' 
             },
             DefaultArguments={
-                # '--job-language': 'python',
+                '--job-language': 'python',
                 '--extra-py-files': extra_files
             },
             Connections={
@@ -26,17 +34,71 @@ def create_glue_job(job_name, script_location, role_arn, glue_client,list_connec
     except Exception as e:
         print("Error creating Glue ETL Job:", str(e))
 
+def update_glue_job(job_name,script_name, script_location, role_arn, glue_client,list_connec,extra_files):
+    try:
+        response = glue_client.update_job(
+            JobName=job_name,
+            JobUpdate={
+                'Role': role_arn,
+                'Command': {
+                    'Name': job_name
+                    'ScriptLocation': script_location,
+                    'PythonVersion': '3'
+                },
+                'DefaultArguments': {
+                    '--job-language': 'python',
+                    '--extra-py-files': extra_files
+                },Connections={
+                    'Connections': list_connec
+                },'WorkerType': 'G.1X',
+                'NumberOfWorkers': 2,
+                'GlueVersion': '2.0',
+                'Timeout': 120,
+            }
+        )
+        print("Glue ETL Job updated successfully:", response['JobName'])
+    except Exception as e:
+        print("Error updating Glue ETL Job:", str(e))
+
+
+
+# def main():
+#     list_connec= ['Redshift connection_trubai_dw']
+#     extra_files = 's3://data-ingestion-bucket-trubai-dev/glue_cicd_automation/utils.zip'
+#     job_name = "automated_glue_job"
+#     script_location = "s3://data-ingestion-bucket-trubai-dev/glue_cicd_automation/main.py" # Replace with your S3 path to the zip folder
+#     role_arn = "arn:aws:iam::311373145380:role/trubai_dev_glue_role" # Replace with your Glue service role ARN
+#     region_name = "us-east-1" # Replace with your AWS region
+
+#     glue_client = boto3.client('glue', region_name=region_name)
+
+#     create_glue_job(job_name, script_location, role_arn, glue_client,list_connec,extra_files)
+
 def main():
     list_connec= ['Redshift connection_trubai_dw']
     extra_files = 's3://data-ingestion-bucket-trubai-dev/glue_cicd_automation/utils.zip'
     job_name = "automated_glue_job"
-    script_location = "s3://data-ingestion-bucket-trubai-dev/glue_cicd_automation/main.py" # Replace with your S3 path to the zip folder
-    role_arn = "arn:aws:iam::311373145380:role/trubai_dev_glue_role" # Replace with your Glue service role ARN
-    region_name = "us-east-1" # Replace with your AWS region
+    script_location = "s3://data-ingestion-bucket-trubai-dev/glue_cicd_automation/main.py"
+    role_arn = "arn:aws:iam::311373145380:role/trubai_dev_glue_role"
+    region_name = "us-east-1"
 
     glue_client = boto3.client('glue', region_name=region_name)
 
-    create_glue_job(job_name, script_location, role_arn, glue_client,list_connec,extra_files)
+    # Check if the job already exists
+    exists, job_info = job_exists(glue_client, job_name)
+
+    if not exists:
+        # Job does not exist, create it
+        create_glue_job(job_name, script_location, role_arn,glue_client,list_connec, extra_files)
+    else:
+        # Job exists, check if the script is updated
+        current_script_location = job_info['Command']['ScriptLocation']
+        if current_script_location != script_location:
+            # Update the job with the new script location
+            update_glue_job(job_name, script_location, role_arn,glue_client,list_connec,extra_files)
+        else:
+            print("Job already exists with the same script. No update needed.")
+
 
 if __name__ == "__main__":
     main()
